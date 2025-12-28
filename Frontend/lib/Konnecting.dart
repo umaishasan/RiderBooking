de85a74a17f509730c4219a05539c538e5a5bac4
DESKTOP-F726TKR\PC <umaishasan96@gmail.com>
2025-12-28 23:55:11 +0500

    Add http in package, add script

---
+++ Frontend/lib/Konnecting.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Konnecting extends StatelessWidget {
+  const Konnecting({super.key});
+  final String serverUrl = 'https://localhost:3000';
+  final String apiRoute = 'auth/v1/items';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
